117c45c032c53dd2caa58690af0c42797d082123
ajbodev <aj.bo.dev@gmail.com>
2016-07-26 10:18:19 -0400

    rcx 0.49.1

--- data/try/data.js
+++ data/try/data.js
@@ -2,11 +2,27 @@ data({
   "data": [
     {
       "name": "lklk",
-      "input_html": "{\"index\":{\"name\":\"index\",\"ext\":\"html\",\"mode\":\"text/html\",\"src\":\"<!DOCTYPE html>\\n<html>\\n  <head>\\n    <style data-include='index'></style>\\n  </head>\\n  <body>Please please \\n    <div id='app'></div>\\n    <script data-include='index'></script>\\n  </body>\\n</html>\"}}",
+      "input_html": "{\"index\":{\"name\":\"index\",\"ext\":\"html\",\"mode\":\"text/html\",\"src\":\"<!DOCTYPE html>\\n<html>\\n  <head>\\n    <style data-include='index'></style>\\n  </head>\\n  <body>Please please NoMOOORERERE\\n    <div id='app'></div>\\n    <script data-include='index'></script>\\n  </body>\\n</html>\"}}",
       "input_css": "{\"index\":{\"name\":\"index\",\"ext\":\"css\",\"mode\":\"text/css\",\"src\":\"\\n#app {\\n  background: red;\\n  height: 50px;\\n  width: 50px;\\n}\\n\"}}",
       "input_js": "{\"index\":{\"name\":\"index\",\"ext\":\"js\",\"mode\":\"text/javascript\",\"src\":\"\\nalert(1);\\n\"}}",
       "date": "23/07/2016",
       "id": 1
+    },
+    {
+      "name": "pop",
+      "input_html": "{\"index\":{\"name\":\"index\",\"ext\":\"jade\",\"mode\":\"text/x-jade\",\"src\":\"doctype html\\nhtml\\n  body adas \\n    div#app\\n    script(data-include='index')\"},\"iioioio\":{\"name\":\"iioioio\",\"ext\":\"html\",\"mode\":\"text/html\",\"src\":\"\\n\\n\"}}",
+      "input_css": "{\"index\":{\"name\":\"index\",\"ext\":\"css\",\"mode\":\"text/css\",\"src\":\"\\n#app {\\n  background: red;\\n  height: 50px;\\n  width: 50px;\\n}\\n\"}}",
+      "input_js": "{\"index\":{\"name\":\"index\",\"ext\":\"js\",\"mode\":\"text/javascript\",\"src\":\"\\nalert(1);\\n\"}}",
+      "date": "23/07/2016",
+      "id": 2
+    },
+    {
+      "name": "popop",
+      "input_html": "{\"index\":{\"name\":\"index\",\"ext\":\"jade\",\"mode\":\"text/x-jade\",\"src\":\"doctype html\\nhtml\\n  body adas\\n    div#app\\n    script(data-include='index')\"},\"iioioio\":{\"name\":\"iioioio\",\"ext\":\"html\",\"mode\":\"text/html\",\"src\":\"\\n\\n\"}}",
+      "input_css": "{\"index\":{\"name\":\"index\",\"ext\":\"css\",\"mode\":\"text/css\",\"src\":\"\\n#app {\\n  background: red;\\n  height: 50px;\\n  width: 50px;\\n}\\n\"}}",
+      "input_js": "{\"index\":{\"name\":\"index\",\"ext\":\"js\",\"mode\":\"text/javascript\",\"src\":\"\\nalert(1);\\n\"}}",
+      "date": "26/07/2016",
+      "id": 3
     }
   ]
 })
