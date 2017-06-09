41ee9bb6a70ccc51058036afb46b91d941c9c447
ajbodev <aj.bo.dev@gmail.com>
2017-06-08 21:29:03 -0400

    Change config of website

--- app/config.js
+++ app/config.js
@@ -3,33 +3,47 @@ __require('app/config', function(require, module, exports) {
 
 var config = {
   home: {
-    tech_1:       {content: 'more'},
-    tech_2:       {content: 'file'},
-  },
-  content: {
-    'more': {
-      title:      'More!',
-      tags:       ['database', 'php', 'more'],
-      date:       '09/02/2017',
-      template:   'Post', //comments: true,
-      start:      50,
-    },
-    'file': {
-      title:      'File',
-      tags:       ['javascript', 'php', 'external'],
-      date:       '07/03/2017',
-      template:   'Post', //comments: true,
-    },
-    'some': {
-      title:      'Some',
-      tags:       ['javascript', 'some'],
-      date:       '08/08/2017',
-      template:   'Post', //comments: true,
-    },
+    tech_1:       {content: 'react-plain-es5'},
+    tech_2:       {content: 'php-arrays-as-pseudo-classes'},
+    news:         {content: 'news-06-2017'},
+    more_1:       {content: 'lessons-in-building-enterprise-software-part-1'},
+    more_2:       {content: 'on-the-web-part-1'},
   },
   tag: {
     'javascript': { title: 'JavaScript', content: 'tag/javascript' },
     'php':        { title: 'PHP',        content: false },
+  },
+  content: {
+    'react-plain-es5': {
+      title:      'React.js in Plain ES5',
+      tags:       ['javascript', 'react'],
+      template:   'Post', 
+      //date:       '08/08/2017', //comments: true,
+    },
+    'php-arrays-as-pseudo-classes': {
+      title:      'PHP Arrays as Pseudo Classes',
+      tags:       ['php'],
+      template:   'Post', 
+      //date:       '08/08/2017', //comments: true,
+    },
+    'news': {
+      title:      'June 2017',
+      tags:       ['news'],
+      template:   'Post', 
+      //date:       '08/08/2017', //comments: true,
+    },
+    'lessons-in-building-enterprise-software-part-1': {
+      title:      'Lessons in Building an Enterprise Software',
+      tags:       ['web'],
+      template:   'Post', 
+      //date:       '08/08/2017', //comments: true,
+    },
+    'on-the-web-part-1': {
+      title:      'On The Web',
+      tags:       ['web'],
+      template:   'Post', 
+      //date:       '08/08/2017', //comments: true,
+    },
   },
 }
 
--- app/content/tech/react-plain-es5.md
+++ app/content/tech/react-plain-es5.md
@@ -2,7 +2,7 @@ __c(function(){/*!
 
 # React in Plain ES5
 
-
+It ever since the 1500s, when an unknown printer took a galley of type a  ls simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type e printin
 
 [//]: # (@~`react-plain-es5`~@)
 
