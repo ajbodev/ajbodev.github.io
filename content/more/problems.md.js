1abf16c7818c665855937d6ecd8e263bcb637ab3
unknown <aj.bo.dev@gmail.com>
2018-12-26 22:16:02 -0500

    Add problems

---
+++ content/more/problems.md.js
@@ -0,0 +1,29 @@
+
+;__c(function(){/*!
+
+# Problems
+
+## Dec 23 - 29
+
+* [ ] Fullstack TypeScript
+
+## Dec 16 - 22
+
+* [x] Rewrite blog code
+
+## Dec 9 - 15
+
+* [x] Ember google maps
+
+<!--
+* barzan
+  * Dec 9 - 15 : Ember google maps
+  * Dec 16 - 22 : Rewrite blog
+  * Dec 23 - 29 : Fullstack TypeScript
+-->
+
+Thanks also Barzan!
+
+[//]: # (@~|more/problems|~@)
+
+*/});
