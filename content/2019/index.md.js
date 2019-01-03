4c7836b070ac2c3a4011133d451a9e69b838df1d
unknown <aj.bo.dev@gmail.com>
2019-01-03 05:50:21 -0500

    Add content to 2019

--- content/2019/index.md.js
+++ content/2019/index.md.js
@@ -3,6 +3,14 @@
 
 # 2019
 
+## Jan 3
+
+* [100 C# LINQ converted to python](https://old.reddit.com/r/programming/comments/ac3ozx/for_the_holidays_i_wanted_to_learn_python_so_i/)
+
+## Jan 2
+
+* [Interesting book about algorithms](https://old.reddit.com/r/programming/comments/abs2hp/an_interesting_book_about_algorithms_algorithms/)
+
 <!-- news -->
 
 [//]: # (@~|2019/index|~@)
