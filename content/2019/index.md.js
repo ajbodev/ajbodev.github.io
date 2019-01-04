fa4f9aaf201f2c0ed297c9539b00d9a12fcbdbeb
unknown <aj.bo.dev@gmail.com>
2019-01-04 08:51:09 -0500

    Add 2019 content

--- content/2019/index.md.js
+++ content/2019/index.md.js
@@ -3,6 +3,11 @@
 
 # 2019
 
+## Jan 4
+
+* [big ol’ ball o’ javascript](https://old.reddit.com/r/programming/comments/acfwwk/big_ol_ball_o_javascript/)
+  * [Reluctant Gatekeeping](http://www.heydonworks.com/article/reluctant-gatekeeping-the-problem-with-full-stack)
+
 ## Jan 3
 
 * [100 C# LINQ converted to python](https://old.reddit.com/r/programming/comments/ac3ozx/for_the_holidays_i_wanted_to_learn_python_so_i/)
@@ -11,7 +16,11 @@
 
 * [Interesting book about algorithms](https://old.reddit.com/r/programming/comments/abs2hp/an_interesting_book_about_algorithms_algorithms/)
 
-<!-- news -->
+<!-- 
+news
+
+* [title](link)
+-->
 
 [//]: # (@~|2019/index|~@)
 
