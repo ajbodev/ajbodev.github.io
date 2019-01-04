1b43053522481238ea035e0f86ed6d6deb995958
unknown <aj.bo.dev@gmail.com>
2019-01-04 14:06:45 -0500

    Add to 2019

--- content/2019/index.md.js
+++ content/2019/index.md.js
@@ -3,10 +3,20 @@
 
 # 2019
 
+<!-- 
+news
+
+* [title](link)
+-->
+
 ## Jan 4
 
 * [big ol’ ball o’ javascript](https://old.reddit.com/r/programming/comments/acfwwk/big_ol_ball_o_javascript/)
   * [Reluctant Gatekeeping](http://www.heydonworks.com/article/reluctant-gatekeeping-the-problem-with-full-stack)
+
+* [Software Engineering at Google](https://old.reddit.com/r/programming/comments/acipmi/software_engineering_at_google/)
+  * [arxiv](https://arxiv.org/ftp/arxiv/papers/1702/1702.01715.pdf)
+  * [(Related) SRE Book(s)](https://landing.google.com/sre/sre-book/toc/index.html)
 
 ## Jan 3
 
