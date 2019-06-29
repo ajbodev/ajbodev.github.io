c39b1be8475fb9718ad2aa577edc224188e3603a
unknown <aj.bo.dev@gmail.com>
2019-06-28 23:02:28 -0400

    Fix custom/bundle/_react/_bundle.react.global.js

--- custom/bundle/_react/_bundle.react.global.js
+++ custom/bundle/_react/_bundle.react.global.js
@@ -5,7 +5,14 @@
 
 React.__spread = Object.assign;
 h              = React.createElement;
-jsx            = (t) => JSXTransformer.transform(t[0]).code;
+//jsx            = (t) => JSXTransformer.transform(t[0]).code;
+
+jsx = function(t) {
+  var _t = t[0];
+  if (jsx.class_replace) _t = t[0].replace(/class=/g, 'className=');
+  return JSXTransformer.transform(_t).code;
+}
+jsx.class_replace = true;
 
 ////////////////////////////////////////////////////////////////////////////////
 
