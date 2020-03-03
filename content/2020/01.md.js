f642b9947968b85f0704e4a561505de2a0a2ee37
ajbodev <aj.bo.dev@gmail.com>
2020-03-02 23:22:48 -0500

    Add to Mar 2020

--- content/2020/01.md.js
+++ content/2020/01.md.js
@@ -3,6 +3,14 @@
 
 # Mar 2020
 
+## Mar 2
+
+* [Organizational skills > Algorithmic wizardry](https://old.reddit.com/r/programming/comments/fc723b/organizational_skills_beat_algorithmic_wizardry/)
+
+  * [Link](https://prog21.dadgum.com/177.html)
+
+## 
+
 * (.. cultural turn ..)
 
 [//]: # (@~|2020/03|~@)
