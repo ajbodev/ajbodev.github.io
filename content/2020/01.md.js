bc0c2ad7570b006f0ff077a622ddb002e65bf819
ajbodev <aj.bo.dev@gmail.com>
2020-03-03 23:08:21 -0500

    Add more to 2020

--- content/2020/01.md.js
+++ content/2020/01.md.js
@@ -3,6 +3,12 @@
 
 # Mar 2020
 
+## Mar 3
+
+* [Programming principles from id Software](https://old.reddit.com/r/programming/comments/fcxj9n/programming_principles_from_id_software/)
+
+  * [Link](https://medium.com/@aloukissas/programming-principles-from-id-software-bed83e762210)
+
 ## Mar 2
 
 * [Organizational skills > Algorithmic wizardry](https://old.reddit.com/r/programming/comments/fc723b/organizational_skills_beat_algorithmic_wizardry/)
